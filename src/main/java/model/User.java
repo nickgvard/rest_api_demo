1f9cd496e285d2bbad4f899b761b7bcf7a940ebe
nickgvard <project.3players@gmail.com>
2021-12-08 21:03:40 +0300

    Created GET method User model. Travis init

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -9,7 +9,6 @@ import java.util.List;
  * email gvardeev@po-korf.ru
  */
 
-@NoArgsConstructor
 @Builder
 @Getter
 @Setter
--- src/main/java/servlet/UserServlet.java
+++ src/main/java/servlet/UserServlet.java
@@ -1,5 +1,6 @@
 package servlet;
 
+import com.google.gson.Gson;
 import controller.UserController;
 import model.User;
 
@@ -9,6 +10,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import static javax.servlet.http.HttpServletResponse.*;
 
 /**
  * @author Nikita Gvardeev
@@ -16,6 +21,7 @@ import java.io.IOException;
  */
 public class UserServlet extends HttpServlet {
 
+    private static final Gson GSON = new Gson();
     private UserController userController;
 
     @Override
@@ -26,7 +32,36 @@ public class UserServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        
+        String requestUrl = req.getRequestURI();
+
+        //getById();
+        if (Pattern.compile("/users/\\d+$").matcher(requestUrl).find()) {
+            Long id = Long.parseLong(requestUrl.substring("/users/".length()));
+
+            User user = userController.getById(id);
+
+            if (user != null) {
+                String json = GSON.toJson(user);
+
+                resp.setStatus(SC_OK);
+                resp.setContentType("application/json");
+                resp.getOutputStream().println(json);
+            } else
+                resp.setStatus(SC_NO_CONTENT);
+        }//findAll();
+        else if (Pattern.compile("/users$").matcher(requestUrl).find()) {
+            List<User> users = userController.findAll();
+
+            if (!users.isEmpty()) {
+                String json = GSON.toJson(users);
+
+                resp.setStatus(SC_OK);
+                resp.setContentType("application/json");
+                resp.getOutputStream().println(json);
+            } else
+                resp.setStatus(SC_NO_CONTENT);
+        } else
+            resp.setStatus(SC_BAD_REQUEST);
     }
 
     @Override
